40f6f2291fa42b0ee8a00ddfc8b60fd69898a3e9
Makis Otman <makisotman@gmail.com>
2014-06-23 15:50:48 +0100

    It converts 1

--- roman_converter.rb
+++ roman_converter.rb
@@ -1,6 +1,9 @@
 class RomanConverter
 
   def self.convert(number)
-    ''
+    if number.zero?
+      return ''
+    end
+    'I'
   end
 end
--- roman_converter_spec.rb
+++ roman_converter_spec.rb
@@ -4,4 +4,8 @@ describe RomanConverter do
   it 'converts 0' do
     expect(RomanConverter.convert(0)).to eq ''
   end
+
+  it 'converts 1' do
+    expect(RomanConverter.convert(1)).to eq 'I'
+  end
 end

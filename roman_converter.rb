f5f3434651a841bbeda864437dbf4b7bf4504a21
Makis Otman <makisotman@gmail.com>
2014-06-23 16:14:04 +0100

    It converts 100

--- roman_converter.rb
+++ roman_converter.rb
@@ -1,12 +1,13 @@
 class RomanConverter
 
-  CONVERSION_TABLE = { 50 => 'L',
-                       40 => 'XL',
-                       10 => 'X',
-                       9  => 'IX',
-                       5  => 'V',
-                       4  => 'IV',
-                       1  => 'I' }
+  CONVERSION_TABLE = { 100 => 'C',
+                       50  => 'L',
+                       40  => 'XL',
+                       10  => 'X',
+                       9   => 'IX',
+                       5   => 'V',
+                       4   => 'IV',
+                       1   => 'I' }
 
   def self.convert(number)
     roman = ''
--- roman_converter_spec.rb
+++ roman_converter_spec.rb
@@ -2,17 +2,18 @@ require_relative 'roman_converter'
 
 describe RomanConverter do
 
-  { 0  => '',
-    1  => 'I',
-    2  => 'II',
-    4  => 'IV',
-    5  => 'V',
-    6  => 'VI',
-    9  => 'IX',
-    10 => 'X',
-    20 => 'XX',
-    40 => 'XL',
-    50 => 'L'
+  { 0   => '',
+    1   => 'I',
+    2   => 'II',
+    4   => 'IV',
+    5   => 'V',
+    6   => 'VI',
+    9   => 'IX',
+    10  => 'X',
+    20  => 'XX',
+    40  => 'XL',
+    50  => 'L',
+    100 => 'C'
   }.each do |arabic_numeral, roman_numeral|
     it "converts #{arabic_numeral}" do
       expect(RomanConverter.convert(arabic_numeral)).to eq roman_numeral

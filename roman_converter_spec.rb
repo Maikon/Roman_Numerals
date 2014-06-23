34977a138d042bfc3cc340a30a38c4483a05782a
Makis Otman <makisotman@gmail.com>
2014-06-23 15:55:07 +0100

    Refactored tests to use a loop

--- roman_converter_spec.rb
+++ roman_converter_spec.rb
@@ -1,15 +1,13 @@
 require_relative 'roman_converter'
 
 describe RomanConverter do
-  it 'converts 0' do
-    expect(RomanConverter.convert(0)).to eq ''
-  end
 
-  it 'converts 1' do
-    expect(RomanConverter.convert(1)).to eq 'I'
-  end
-
-  it 'converts 2' do
-    expect(RomanConverter.convert(2)).to eq 'II'
+  { 0 => '',
+    1 => 'I',
+    2 => 'II'
+  }.each do |arabic_numeral, roman_numeral|
+    it "converts #{arabic_numeral}" do
+      expect(RomanConverter.convert(arabic_numeral)).to eq roman_numeral
+    end
   end
 end
